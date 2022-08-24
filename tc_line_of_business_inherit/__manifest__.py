d355b27797f940ec245422d1d9a4d0690fdb162b
Ali_Chamsedine <alichamsedine7@gmail.com>
2022-08-24 12:23:25 +0300

    First task

---
+++ tc_line_of_business_inherit/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Line of Business Inherit",
+    "summary": "Line of business",
+    "author": "Daniel Reis",
+    "license": "AGPL-3",
+    "website": "https://github.com/PacktPublishing"
+               "/Odoo-15-Development-Essentials",
+    "version": "15.0.1.0.0",
+    "category": "Services/Library",
+    "depends": ["base","contacts"],
+    "data": [
+        "views/business_contact_inherit.xml",
+    ],
+}
---
+++ tc_line_of_business_inherit/models/__init__.py
@@ -0,0 +1,1 @@
+from . import line_of_business_inherit
