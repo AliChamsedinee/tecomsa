e77347a0dd89822b4c91713b64cfcacc0dc817f9
Ali_Chamsedine <alichamsedine7@gmail.com>
2022-08-24 16:25:56 +0300

    Time off report

---
+++ tc_time_off_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Time off report",
+    "summary": "Time off report",
+    "author": "Daniel Reis",
+    "license": "AGPL-3",
+    "website": "https://github.com/PacktPublishing"
+               "/Odoo-15-Development-Essentials",
+    "version": "15.0.1.0.0",
+    "category": "Services/Library",
+    "depends": ["base","hr_holidays"],
+    "data": [
+        # "views/business_contact_inherit.xml",
+        "reports/report.xml",
+        "reports/time_off_report.xml",
+    ],
+}
