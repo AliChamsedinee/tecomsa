f57052bd725b009bc77634d5a858d5108800598e
Ali_Chamsedine <alichamsedine7@gmail.com>
2022-09-15 12:03:14 +0300

    Line of business

---
+++ tc_line_of_business/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Line of Business",
+    "summary": "Manage library catalog and book lending.",
+    "author": "Daniel Reis",
+    "license": "AGPL-3",
+    "website": "https://github.com/PacktPublishing"
+               "/Odoo-15-Development-Essentials",
+    "version": "15.0.1.0.0",
+    "category": "Services/Library",
+    "depends": ["base","contacts"],
+    "data": [
+        "views/line_of_business_menu.xml",
+        "views/line_of_business_view.xml",
+    ],
+}
